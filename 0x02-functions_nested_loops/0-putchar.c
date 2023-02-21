ec0977bf44b8e523e120a22ad13d6946a19ccfb5
samit1719 <samuelteshome1719@gmail.com>
2023-02-21 10:54:58 -0800

    updated files

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * text
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
-
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	char *sh = "_putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
