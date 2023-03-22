0a90536a22e354b72d9ead4a1c58c29b14b8420c
samit1719 <samuelteshome1719@gmail.com>
2023-03-22 03:30:45 -0700

    up

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,25 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
-
 /**
- * get_op_func -  selects the correct function
- * @s: input string
- * Return: pointer to a function
+ * get_op_func - matches operator from main
+ * @s: op str
+ * Return: 0
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
+	op_t op_s[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
+		{NULL, NULL}
+	};
+
 	int i = 0;
 
-	while (i < 5)
+	while (op_s[i].op)
 	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
-			return (ops[i].f);
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
 		i++;
 	}
 	return (NULL);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,59 +1,51 @@
 #include "3-calc.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * op_add - adds two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the sum
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtracts two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the difference
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - multiplies two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the product
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - divides two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the quotient
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod -  modulo two integers
- * @a: the first int
- * @b: the second int
- *
- * Return: the remainder
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
  */
 int op_mod(int a, int b)
 {
